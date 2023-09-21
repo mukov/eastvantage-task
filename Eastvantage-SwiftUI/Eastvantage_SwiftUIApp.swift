b3ee7d8d0d9c17a46a75d6428a5f9379fdc9ed60
mukov <alexander.mukov@gmail.com>
2023-09-21 17:02:02 +0300

    Initial Commit

---
+++ Eastvantage-SwiftUI/Eastvantage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eastvantage_SwiftUIApp.swift
+//  Eastvantage-SwiftUI
+//
+//  Created by mukov on 21.09.23.
+//
+
+import SwiftUI
+
+@main
+struct Eastvantage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
